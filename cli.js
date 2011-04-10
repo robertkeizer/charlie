381e17ef4b32214b6adc041ce1ebaec85ebe96fb
Robert Keizer <robert@keizer.ca>
2011-04-09 21:05:24 -0500

    Added doCommand and findInPath functions.

--- cli.js
+++ cli.js
@@ -2,6 +2,8 @@
 
 // Include required modules..
 var sys		= require( 'sys' );
+var fs		= require( 'fs' );
+var path	= require( 'path' );
 
 exports.cli	= function( pipedInput, environment, arguments ){
 	
@@ -21,8 +23,15 @@ exports.cli	= function( pipedInput, environment, arguments ){
 		// Remove the trailing \n.. 
 		var escapedChunk = chunk.toString().replace( RegExp( "\n$" ), "" );
 
-		
-		process.stdout.write( "Got chunk of '" + escapedChunk + "'" );
+		// For loop if semicolon's detected.
+		var splitEscapedChunk = escapedChunk.split( ';' );
+		if( splitEscapedChunk.length > 1 ){
+			for( var x=0; x<splitEscapedChunk.length; x++ ){
+				doCommand( splitEscapedChunk[x] );
+			}
+		}else{
+			doCommand( escapedChunk );
+		}
 
 		// Show the prompt again.
 		showPrompt( );
@@ -31,10 +40,46 @@ exports.cli	= function( pipedInput, environment, arguments ){
 	process.stdin.on( 'end', function( ){
 		process.stdout.write( "\nGoodbye.\n" );
 	} );
+
+	// Actually do the command.. ( parse and Execute the command - showing its output to process.stdout.. ).
+	function doCommand( commandInput ){
+		var commandInputSplitBySpace	= commandInput.split( " " );
+		var firstCommand		= commandInputSplitBySpace[0];
+		
+		var pathToFirstCommand		= findInPath( firstCommand );
+		if( !pathToFirstCommand ){
+			process.stdout.write( "Command '" + firstCommand + "' was not found.." );
+			return;
+		}
+		
+		process.stdout.write( "Would require file '" + pathToFirstCommand + "' .. " );
+		return;
+	}
+
+	// Find a file in the environment['PATH'] object's directories.. 
+	// If fileToFind doesn't have a '.js' suffix, add one.. 
+	function findInPath( fileToFind ){
+		if( !fileToFind.match( "\.js$" ) ){ fileToFind += ".js" };
+
+		var paths	= environment["PATH"].split( ":" );
+		for( var x=0; x<paths.length; x++ ){
+			try{
+				var pathContent	= fs.readdirSync( paths[x] );
+				for( var y=0; y<pathContent.length; y++ ){
+					if( fileToFind == pathContent[y] ){
+						return path.join( paths[x], pathContent[y] );
+					}
+				}
+			}catch( err ){
+				return false;
+			}
+		}
+
+		return false;
+	}
 }
 
 var environmentObj	= Array( );
 environmentObj["PATH"]	= ".:bin/";
-
 
 exports.cli( "", environmentObj, "" );
