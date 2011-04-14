5fff41bc1cb98176d8ad2b3483c49d6ef1abee3c
Robert Keizer <robert@keizer.ca>
2011-04-14 14:10:41 -0500

    Updated ls.js - better foreach loop.

--- bin/ls.js
+++ bin/ls.js
@@ -10,23 +10,17 @@ exports.ls = function( pipedInput, inputArgs, detail ){
 	var inputArgsSplitBySpace	= inputArgs.split( " " );
 
 	if( inputArgsSplitBySpace.length > 1 ){
-
-		for( var x=0; x<inputArgsSplitBySpace.length; x++ ){
-			returnString += exports.ls( pipedInput, inputArgsSplitBySpace[x] );
-		}
+		inputArgsSplitBySpace.forEach( function( inputArg ){
+			returnString += exports.ls( pipedInput, inputArg, detail );
+		} );
 	}else{
 		try{
 			var dircontents	= fs.readdirSync( inputArgs );
-			for( var y=0; y<dircontents.length; y++ ){
+			dircontents.forEach( function( dirContent ){
+				returnString += dirContent + "\n";
+			} );
+			returnString	= returnString.trim();
 
-				// dircontents[y] contains one entry of the directory such as 
-				// a file or another directory... 
-				
-				returnString += dircontents[y];
-				if( y != dircontents.length -1 ){
-					returnString += "\n";
-				}
-			}
 		}catch(err){
 			return err;
 		}
