ee2a4086f9217a82c1b066c431c64b152717b484
Robert Keizer <robert@keizer.ca>
2011-04-15 14:43:59 -0500

    Re-wrote guts of cli.js.

--- cli.js
+++ cli.js
@@ -12,125 +12,96 @@ exports.cli	= function( pipedInput, arguments ){
 	// Resume the stdin readable stream..
 	process.stdin.resume( );
 	
-	// A function to show the prompt..
-	function showPrompt( first ){
-		if( first ){
-			process.stdout.write( "cli>" );
-		}else{
-			process.stdout.write( "\ncli>" );
-		}
+	// A function to drop a line..
+	function dropLine( ){
+		process.stdout.write( "\n" );
 	}
 
-	// Show the prompt for the first time.. 
-	showPrompt( true );
+	// A function to show the prompt..
+	function showPrompt( ){
+		process.stdout.write( "cli>" );
+	}
+
+	showPrompt( );
 
 	// When a line is recieved..
 	process.stdin.on( 'data', function( chunk ){
 
-		var showPromptDashN	= false;
-		
-		// Remove the trailing \n.. 
-		var escapedChunk = chunk.toString().replace( RegExp( "\n$" ), "" );
+		var chunkToUse	= chunk.toString( ).replace( RegExp( "\n$" ), "" );
 
-		// For loop if semicolon's detected.
-		var splitEscapedChunk = escapedChunk.split( ';' );
-		if( splitEscapedChunk.length > 1 ){
-			for( var x=0; x<splitEscapedChunk.length; x++ ){
-				doCommand( splitEscapedChunk[x] );
-			}
-		}else{
-			// Don't bother executing doCommand for nothing..
-			if( escapedChunk == "" ){
-				showPromptDashN = true;
-			}else{
-				doCommand( escapedChunk );
-			}
+		if( chunkToUse == "" ){
+			showPrompt( );
+			return;
 		}
 
-		// Show the prompt again.
-		showPrompt( showPromptDashN );
+		var chunkSplitSemi	= chunkToUse.split( ';' );
+		if( chunkSplitSemi.length > 1 ){
+			chunkSplitSemi.forEach( function( chunkPart ){
+				executeCommand( chunkPart );
+			} );
+		}else{
+			executeCommand( chunkToUse );
+		}
+
+		showPrompt( );
 	} );
 
-	process.stdin.on( 'end', function( ){ process.stdout.write("\n"); } );
+	process.stdin.on( 'end', function( ){
+		dropLine( );
+		process.stdout.write( "Bye." );
+		dropLine( );
+	} );
 
-	// Actually do the command.. ( parse and Execute the command - showing its output to process.stdout.. ).
-	function doCommand( commandInput ){
-
-		// Remove spaces at the beginning and end of the command..
+	function executeCommand( commandInput ){
 		commandInput	= commandInput.trim();
+	
+		if( commandInput.split( "|" ).length > 1 ){
+			process.stdout.write( "Piping is not yet implemented." );
+			dropLine( );
+			return;
+		}	
 
-		// Split by semicolons and recurse..
-		var semiSplit	= commandInput.split( ";" );
-		if( semiSplit.length > 1 ){
-			for( var x=0; x<semiSplit.length; x++ ){
-				if( semiSplit[x].trim() != "" ){
-					doCommand( semiSplit[x] );
-				}
-			}
+		var commandSplit	= commandInput.split( " " );
+		var pathToFile		= findInPath( commandSplit[0] );
+		if( pathToFile == false ){
+			process.stdout.write( "The command '" + commandSplit[0] + "' was not found." );
+			dropLine( );
+			return;
 		}
 
-		// Piping.. 
-		var fullCmd	= "";
-		var pipeSplit	= commandInput.split( "|" );
-		for( var x=0; x<pipeSplit.length; x++ ){
-			var tmpPipeSplitBySpace		= pipeSplit[x].trim().split( " " );
-			var tmpPipeFirstCommand		= tmpPipeSplitBySpace[0].trim();
-			var tmpPipeArguments		= pipeSplit[x].trim().replace( RegExp( "^" + tmpPipeFirstCommand ), '' ).trim();
-
-			var tmpPathToFirstCommand	= findInPath( tmpPipeFirstCommand );
-			if( !tmpPathToFirstCommand ){
-				process.stdout.write( "Command '" + tmpPipeFirstCommand + "' was not found." );
-				return;
-			}
-
-			// First time around, make sure to include black quotes for input args..
-			if( fullCmd == "" ){
-				fullCmd = "require( '" + tmpPathToFirstCommand + "' )." + tmpPipeFirstCommand + "( '', '" + tmpPipeArguments + "' )";
-			}else{
-				fullCmd	= "require( '" + tmpPathToFirstCommand + "' )." + tmpPipeFirstCommand + "( " + fullCmd;
-				fullCmd = fullCmd + ", '" + tmpPipeArguments + "' )";
-			}
-		}
-
-		// Ugly as heck.. 
-
-		process.stdout.write( "" + 
-			eval( fullCmd )
-		);
-		return;
-	}
-
-	// Find a file in the environment['PATH'] object's directories.. 
-	// If fileToFind doesn't have a '.js' suffix, add one.. 
-	function findInPath( fileToFind ){
-		if( !fileToFind.match( "\.js$" ) ){ fileToFind += ".js" };
-
-		var paths	= environment["PATH"].split( ":" );
-		for( var x=0; x<paths.length; x++ ){
-			try{
-				var pathContent	= fs.readdirSync( paths[x] );
-				for( var y=0; y<pathContent.length; y++ ){
-					if( fileToFind == pathContent[y] ){
-						var fullpath	= path.join( paths[x], pathContent[y] );
-						if( !fullpath.match( RegExp( "^\." ) ) || !fullpath.match( RegExp( '^' + '/' ) ) ){
-							fullpath = "./" + fullpath;
-						}
-						return fullpath;
-					}
-				}
-			}catch( err ){
-				return false;
-			}
-		}
-
-		return false;
+		console.log( "Got this far.. with pathToFile of '" + pathToFile + "'" );
 	}
 }
 
 // Grab the absolute path to this cli.js, set paths accordingly.. 
 var absoluteDirPath	= process.argv[1].replace( RegExp( "\/cli\.js$" ), "" );
 
+// Create an environment variable.
 environment		= Array( );
 environment["PATH"]	= absoluteDirPath + ":" + absoluteDirPath + "/bin/";
 environment["PWD"]	= process.cwd();
+
+// A function to look through environment[PATH] and find a specific file.
+function findInPath( fileToFind ){
+	var returnArray	= Array( );
+	environment["PATH"].split( ":" ).forEach( function( pathToSearch ){
+		try{
+			fs.readdirSync( pathToSearch ).forEach( function( pathContent ){
+				if( pathContent == fileToFind ){
+					returnArray.push( path.join( pathToSearch, pathContent ) );
+				}
+			} );
+		}catch( err ){
+			// Do nothing with the error..
+		}
+	} );
+
+	if( returnArray.length == 0 ){
+		return false;
+	}else{
+		return returnArray;
+	}
+}
+
+// Start the cli.
 exports.cli( "", "" );
