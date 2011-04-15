548c7f114c9720088b54e4653f998fc7ff5a4d09
Robert Keizer <robert@keizer.ca>
2011-04-15 12:27:21 -0500

    Added detailed ps..

--- bin/ps.js
+++ bin/ps.js
@@ -1,8 +1,20 @@
 var linux	= require( 'linux-util' );
 exports.ps	= function( pipedInput, inArgs ){
-	var returnString	= "PID\tUID\tGID\tState\t\tCommand\n";
+	var detail = false;
+	if( inArgs.match( "x" ) ){
+		detail = true;
+	}
+	var returnString	= "PID\t";
+	if( detail ){
+		returnString += "UID\tGID\tState";
+	}
+	returnString += "\tCommand\n";
+
 	linux.ps().forEach( function( psObj ){
-		returnString += psObj['pid'] + "\t" + psObj['uid'] + "\t" + psObj['gid'] + "\t" + psObj['state'] + "\t";
+		returnString += psObj['pid'] + "\t";
+		if( detail ){
+			returnString += psObj['uid'] + "\t" + psObj['gid'] + "\t" + psObj['state'] + "\t";
+		}
 		if( psObj['cmdline'] ){
 			returnString += psObj['cmdline'].substr(0,80) + "\n";
 		}else{
